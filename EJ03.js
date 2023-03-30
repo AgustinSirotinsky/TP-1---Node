47edc0dde55b431ee705feac69f470b71bfdf740
AGUSTÍN SIROTINSKY <46645963@ort>
2023-03-30 10:29:19 -0300

    30/3.2

---
+++ EJ03.js
@@ -0,0 +1,3 @@
+const alumno=require("./models/alumno.js")
+console.log(`username: ${alumno.username}`)
+console.log(`DNI: ${alumno.DNI}`)
---
+++ models/alumno.js
@@ -0,0 +1,4 @@
+var alumno=new Object();
+alumno.username='ilanelprottelbrill'
+alumno.DNI='472034pp'
+module.exports=alumno
